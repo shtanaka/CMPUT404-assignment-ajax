076c0390644a75f268872e7548e565b808a4f626
Edson Gustavo Santiago Silva <egustavo4@gmail.com>
2016-10-24 17:10:13 -0500

    Printing the Brazilian flag on click

--- server.py
+++ server.py
@@ -11,7 +11,7 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
+# See the License for the spaceecific language governing permissions and
 # limitations under the License.
 #
 # You can start this by executing it in python:
@@ -19,12 +19,17 @@
 #
 # remember to:
 #     pip install flask
+#     pip install -U flask-cors
 
 
 import flask
-from flask import Flask, request
 import json
+from flask import Flask, request, redirect
+#from flask_cors import CORS, cross_origin
+
+
 app = Flask(__name__)
+#cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.debug = True
 
 # An example world
@@ -73,28 +78,27 @@ def flask_post_json():
 
 @app.route("/")
 def hello():
-    '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html", code=302)
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
-    '''update the entities via this interface'''
-    return None
+    e = flask_post_json()
+    for key, value in e.iteritems():
+        myWorld.update(entity, key, value)
+    return flask.jsonify(myWorld.get(entity))
 
-@app.route("/world", methods=['POST','GET'])    
+@app.route("/world", methods=['POST','GET'])  
 def world():
-    '''you should probably return the world here'''
-    return None
+    return flask.jsonify(myWorld.world())
 
-@app.route("/entity/<entity>")    
+@app.route("/entity/<entity>")   
 def get_entity(entity):
-    '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return flask.jsonify(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
-    '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return flask.jsonify(myWorld.world())
 
 if __name__ == "__main__":
     app.run()
